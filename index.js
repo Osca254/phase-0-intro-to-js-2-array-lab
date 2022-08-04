87668994ba83cbbddccc0e4155d64b8ee3b7314f
moringa <oscarokumu47@gmail.com>
2022-08-04 17:51:46 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+   cats.push("Ralph");
+}
+function  destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+
+function  appendCat(name){
+    
+}
+
+
