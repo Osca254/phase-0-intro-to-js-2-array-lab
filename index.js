3d0e4bd502b7074a0225c2086dfca07bc8b4e053
moringa <oscarokumu47@gmail.com>
2022-08-19 15:02:39 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 // Write your solution here!
 let cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name){
-   cats.push("Ralph");
+  return cats.push("Ralph");
 }
 function  destructivelyPrependCat(name){
-    cats.unshift("Bob");
+  return  cats.unshift("Bob");
 }
 function destructivelyRemoveLastCat(){
-    cats.pop('Garfield');
+ return   cats.pop('Garfield');
 }
 function destructivelyRemoveFirstCat(){
-    cats.shift('Milo');
+   return cats.shift('Milo');
 }
 
 function  appendCat(name){
-    
+    return [...cats,name];
 }
+function prependCat(name) {
+  return[name, ...cats];
+}
+ function removeLastCat() {
+   return cats.slice(0,-1);
+ }
 
-
+ function removeFirstCat () {
+   return cats.slice(1)
+ }
